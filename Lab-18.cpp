cbea52ac83bed2330a494cb70c4506592a9f777f
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-09-29 15:37:08 -0700

    3:37 PM

--- Lab-18.cpp
+++ Lab-18.cpp
@@ -2,25 +2,45 @@
 // IDE used: VS Code
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-void output(Node *);
-
-
 struct Node{
-
-
+    double rating;
+    string comment;
+    Node* next;
 };
 
-int main(){
+void output(Node *);
+Node* choice1(Node *);
+Node * choide2(Node *);
 
+int main(){
+    Node *head = nullptr;
+    
+
+    cout<<"Which linked list method should we use?"<<endl;
+    cout<<"    [1] New nodes are added at the head of the linked list"<<endl;
+    cout<<"    [2] New nodes are added at the tail of the linked list"<<endl;
+    cout<<"Choice: ";
+    int choice;
+    cin>>choice;
+    while(true){
+        double rating;
+        string comment;
+        cout<<"Enter review rating 0-5: ";
+        cin>>rating;
+        cout<<"Enter review comments: ";
+        getline(cin,comment);
+        cout<<endl<<comment;
+        break;
+
+
+
+    }
 
 
 
 
     return 0;
-}
-
-void output(Node * head){
-    
 }
